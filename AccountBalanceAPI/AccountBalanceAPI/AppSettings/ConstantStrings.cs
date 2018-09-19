672dfe213332c0cdfcdcb60393ccaa3b0dc7c40d
vinoch <vinochse@jkintranet.com>
2018-09-19 17:14:02 +0000

    changes

--- AccountBalanceAPI/AccountBalanceAPI/AppSettings/ConstantStrings.cs
+++ AccountBalanceAPI/AccountBalanceAPI/AppSettings/ConstantStrings.cs
@@ -4,8 +4,8 @@ namespace AccountBalanceAPI.AppSettings
     public static class ConstantStrings
     {
         #region Application Settings
-        //public static string DBCONNECTIONURL = Environment.GetEnvironmentVariable("DBCONNECTIONURL");
-        public static string DBCONNECTIONURL = "Server=tcp:accountbalance.database.windows.net,1433;Initial Catalog=AccountBalance;Persist Security Info=False;User ID=vinoch;Password=pass#word1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public static string DBCONNECTIONURL = Environment.GetEnvironmentVariable("DBCONNECTIONURL");
+        //public static string DBCONNECTIONURL = "Server=tcp:accountbalance.database.windows.net,1433;Initial Catalog=AccountBalance;Persist Security Info=False;User ID=vinoch;Password=pass#word1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         #endregion
     }
 }
