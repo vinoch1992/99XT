c8d4825a508993d651b29229e9ab9fecf2fe8e99
vinoch <vinochse@jkintranet.com>
2018-09-19 17:32:54 +0000

    code clean

--- AccountBalanceAPI/AccountBalanceAPI/DBConnection/DBConnection.cs
+++ AccountBalanceAPI/AccountBalanceAPI/DBConnection/DBConnection.cs
@@ -10,6 +10,24 @@ namespace AccountBalanceAPI.DBConnection
 
         public DbSet<Users> Users { get; set; }
 
+        private static readonly bool[] _migrated = { false };
+
+        public AccountBalanceContext() : base(GetOptions(ConstantStrings.DBCONNECTIONURL))
+        {
+            if (!_migrated[0])
+                lock (_migrated)
+                    if (!_migrated[0])
+                    {
+                        Database.Migrate(); // apply all migrations
+                        _migrated[0] = true;
+                    }
+        }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(ConstantStrings.DBCONNECTIONURL);
